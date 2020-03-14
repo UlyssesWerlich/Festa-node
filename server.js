4a92a2dec8513865d3dfabbbeb30b8df09be88b7
UlyssesWerlich <werlichborgesulysses@gmail.com>
2020-03-14 14:26:10 -0300

    Commit 002 - feito o cadastro e consulta de clientes, falta editar e excluir cliente

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const router = express.Router()
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-const objectId = require('mongodb').objectId
-/*const uri = ""
-
-MongoClient.connect(uri, (err, client) => {
-    if (err) return console.log(err)
-    db = client('bancoDeDados')
-
-    app.listen( 3000, () =>{
-        console.log('Servidor rodando na porta 3000')
-    })
-})*/
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+mongoose.connect('mongodb://localhost:27017/sistema_aluguel_festa');  
 
 app.listen( 3000, () =>{
     console.log('Servidor rodando na porta 3000')
@@ -21,6 +13,21 @@ app.listen( 3000, () =>{
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
+
+const schemaCliente = new Schema({
+    nome: {type: String, required: true},
+    cpf: String,
+    dataNascimento: String,
+    telefone: String,
+    celular: String,
+    email: String,
+    cep: String,
+    uf: String,
+    cidade: String,
+    endereco: String
+}, {collection: 'clientes'})
+
+var clientes = mongoose.model('UserData', schemaCliente)
 
 app.get('/', (req, res) =>{
     res.render('index.ejs')
@@ -38,9 +45,72 @@ app.get('/clienteCadastro', (req, res) =>{
     res.render('clienteCadastro.ejs')
 })
 
-app.get('/clienteConsulta', (req, res) =>{
-    res.render('clienteConsulta.ejs')
+app.post('/clienteCadastro', (req, res) =>{
+    var cliente = {  
+        nome: req.body.nome,  
+        cpf: req.body.cpf,
+        dataNascimento: req.body.dataNascimento,
+        telefone: req.body.telefone,
+        celular: req.body.celular,
+        email: req.body.email,
+        cep: req.body.cep,
+        uf: req.body.uf,
+        cidade: req.body.cidade,
+        endereco: req.body.endereco
+      };  
+      var data = new clientes(cliente);  
+      data.save().catch((err) => {
+        return console.log(err)
+    }); 
+    res.render('clienteCadastro.ejs')
 })
+
+app.get('/clienteConsulta', (req, res) =>{
+    res.render('clienteConsulta.ejs', {data: false})
+})
+
+app.post('/clienteConsulta', (req, res) =>{  
+    var busca = { "nome": RegExp(req.body.nomeConsulta , 'i')}
+    clientes.find(busca)
+    .then(function(result){
+        res.render('clienteConsulta.ejs', {data: result})
+    }).catch((err) => {
+        return console.log(err)
+        //res.redirect('/clienteConsulta'); 
+    }); 
+})
+
+app.get('/clienteAlterar/:id', (req, res) =>{
+    var busca = { "_id": req.params.id}
+    clientes.find(busca)
+    .then(function(result){
+        res.render('clienteAlterar.ejs', {data: result})
+    }).catch((err) => {
+        return console.log(err)
+    })
+})
+
+app.post('/clienteAlterar/:id', (req, res) =>{
+    var data = {  
+        nome: req.body.nome,  
+        cpf: req.body.cpf,
+        dataNascimento: req.body.dataNascimento,
+        telefone: req.body.telefone,
+        celular: req.body.celular,
+        email: req.body.email,
+        cep: req.body.cep,
+        uf: req.body.uf,
+        cidade: req.body.cidade,
+        endereco: req.body.endereco
+      };  
+    
+    clientes.updateOne({ "_id": req.params.id}, { $set : {data}}).catch((err) => {
+        return console.log(err)
+    }); 
+    res.render('clienteConsulta.ejs', {data: false})
+})
+
+
 
 app.get('/festaCadastro', (req, res) =>{
     res.render('festaCadastro.ejs')
