1e7271f04896bc2b590239ed35e375f75be749d9
UlyssesWerlich <werlichborgesulysses@gmail.com>
2020-03-15 14:49:19 -0300

    Commit 003 - Completado o CRUD de clientes, criado os arquivos produtoAlterar e festaAlterar

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) =>{
+    res.render('index.ejs')
+})
+
+//================================= ROTAS RELACIONADAS A CLIENTES =================================
+
 const schemaCliente = new Schema({
     nome: {type: String, required: true},
     cpf: String,
@@ -28,18 +34,6 @@ const schemaCliente = new Schema({
 }, {collection: 'clientes'})
 
 var clientes = mongoose.model('UserData', schemaCliente)
-
-app.get('/', (req, res) =>{
-    res.render('index.ejs')
-})
-
-app.get('/produtoCadastro', (req, res) =>{
-    res.render('produtoCadastro.ejs')
-})
-
-app.get('/produtoConsulta', (req, res) =>{
-    res.render('produtoConsulta.ejs')
-})
 
 app.get('/clienteCadastro', (req, res) =>{
     res.render('clienteCadastro.ejs')
@@ -58,6 +52,7 @@ app.post('/clienteCadastro', (req, res) =>{
         cidade: req.body.cidade,
         endereco: req.body.endereco
       };  
+
       var data = new clientes(cliente);  
       data.save().catch((err) => {
         return console.log(err)
@@ -90,26 +85,56 @@ app.get('/clienteAlterar/:id', (req, res) =>{
     })
 })
 
-app.post('/clienteAlterar/:id', (req, res) =>{
-    var data = {  
-        nome: req.body.nome,  
-        cpf: req.body.cpf,
-        dataNascimento: req.body.dataNascimento,
-        telefone: req.body.telefone,
-        celular: req.body.celular,
-        email: req.body.email,
-        cep: req.body.cep,
-        uf: req.body.uf,
-        cidade: req.body.cidade,
-        endereco: req.body.endereco
-      };  
+app.post('/clienteAlterar/:id', (req, res) =>{ 
     
-    clientes.updateOne({ "_id": req.params.id}, { $set : {data}}).catch((err) => {
-        return console.log(err)
+    /*cliente = {  
+        $set: {
+            nome: req.body.nome,  
+            cpf: req.body.cpf,
+            dataNascimento: req.body.dataNascimento,
+            telefone: req.body.telefone,
+            celular: req.body.celular,
+            email: req.body.email,
+            cep: req.body.cep,
+            uf: req.body.uf,
+            cidade: req.body.cidade,
+            endereco: req.body.endereco,
+            __v: 0
+        }
+    };  
+    var data = new clientes(cliente);  */
+    
+    clientes.updateOne({ "_id": req.params.id}, { 
+        $set: {
+            nome: req.body.nome,  
+            cpf: req.body.cpf,
+            dataNascimento: req.body.dataNascimento,
+            telefone: req.body.telefone,
+            celular: req.body.celular,
+            email: req.body.email,
+            cep: req.body.cep,
+            uf: req.body.uf,
+            cidade: req.body.cidade,
+            endereco: req.body.endereco,
+            __v: 0
+    }}, (err, result) =>{
+        if (err) return res.send(err)
+        res.render('clienteConsulta.ejs', {data: false})
     }); 
-    res.render('clienteConsulta.ejs', {data: false})
 })
 
+app.get('/clienteExcluir/:id', (req, res) =>{
+    var id = { "_id": req.params.id}
+    clientes.deleteOne(id)
+    .then(function(result){
+        res.render('clienteConsulta.ejs', {data: false})
+    }).catch((err) => {
+        return console.log(err)
+    })
+})
+ 
+
+//================================= ROTAS RELACIONADAS A FESTA =================================
 
 
 app.get('/festaCadastro', (req, res) =>{
@@ -119,6 +144,18 @@ app.get('/festaCadastro', (req, res) =>{
 app.get('/festaConsulta', (req, res) =>{
     res.render('festaConsulta.ejs')
 })
+
+//================================= ROTAS RELACIONADAS A PRODUTOS =================================
+
+app.get('/produtoCadastro', (req, res) =>{
+    res.render('produtoCadastro.ejs')
+})
+
+app.get('/produtoConsulta', (req, res) =>{
+    res.render('produtoConsulta.ejs')
+})
+
+// ================================= ROTAS RELACIONADAS A AGENDA =================================
 
 app.get('/agenda', (req, res) =>{
     res.render('agenda.ejs')
